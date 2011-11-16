f5a4d944178c06b05d4a56e969a724fa38ea0dad
fishtrees@gmail.com <fishtrees@gmail.com@f0fb3338-e2dd-dc1e-c2d3-7b6d7c6fd296>
2011-11-16 13:25:47 +0000

    add basic implementation

---
+++ trunk/src/SharpMQ/SharpMQ/MessageKeyComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpMQ
+{
+    class MessageKeyComparer : IComparer<MessageKey>
+    {
+        public int Compare(MessageKey x, MessageKey y)
+        {
+            int theIdResult = x.Id.CompareTo(y.Id);
+            if (theIdResult.Equals(0))
+            {
+                return 0;
+            }
+            int thePriorityResult = x.Priority.CompareTo(y.Priority);
+            if (thePriorityResult.Equals(0))
+            {
+                return theIdResult;
+            }
+            return thePriorityResult;
+        }
+    }
+}
---
+++ trunk/src/SharpMQ/SharpMQ/MessageStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SharpMQ
+{
+    enum MessageStatus
+    {
+        Ready = 0,
+        Reserved = 1,
+        Deleted = 2,
+        Delayed = 3,
+        Buried = 4
+    }
+}
