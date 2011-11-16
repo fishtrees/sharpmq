28e5928269e3eaae12da12040f46e88a793690ea
fishtrees@gmail.com <fishtrees@gmail.com@f0fb3338-e2dd-dc1e-c2d3-7b6d7c6fd296>
2011-11-16 13:25:47 +0000

    add basic implementation

---
+++ src/SharpMQ/SharpMQ/MessageKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpMQ
+{
+    class MessageKey
+    {
+        public ulong Id { get; set; }
+        public uint Priority { get; set; }
+        public uint Timeout { get; set; }
+        public MessageStatus Status { get; set; }
+
+        public MessageKey(ulong theId, uint thePriority, uint theTimeout, MessageStatus theStatus)
+        {
+            this.Id = theId;
+            this.Priority = thePriority;
+            this.Timeout = theTimeout;
+            this.Status = theStatus;
+        }
+    }
+}
---
+++ src/SharpMQ/SharpMQ/MessageQueue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpTest.Net.Serialization;
+using CSharpTest.Net.Collections;
+using System.IO;
+using CSharpTest.Net.Synchronization;
+
+namespace SharpMQ
+{
+    public class MessageQueue : IDisposable
+    {
+        private BPlusTree<MessageKey, MessageData> _Tree;
+
+        public MessageQueue(string theFilePath)
+        {
+            BPlusTree<MessageKey, MessageData>.Options options
+                = new BPlusTree<MessageKey, MessageData>.Options(
+                    new MessageKeySerializer(),
+                    new MessageDataSerializer(),
+                    new MessageKeyComparer())
+            {
+                CreateFile = CreatePolicy.IfNeeded,
+                FileName = theFilePath,
+                FileBlockSize = 8192,
+                FileOpenOptions = FileOptions.RandomAccess,
+                StorageType = StorageType.Disk,
+                CacheKeepAliveTimeout = 10000,
+                CacheKeepAliveMinimumHistory = 0,
+                CacheKeepAliveMaximumHistory = 3000
+            };
+
+            options.CallLevelLock = new ReaderWriterLocking();
+            options.LockingFactory = new LockFactory<SimpleReadWriteLocking>();
+            options.LockTimeout = 10000;
+            options.ConcurrentWriters = 8;
+
+            this._Tree = new BPlusTree<MessageKey, MessageData>(options);
+        }
+
+        public bool Enqueue(byte[] data, uint priority, uint timeout)
+        {
+            MessageKey msgKey = new MessageKey((ulong)MessageIdManager.Default.GetNextId(), priority, timeout, MessageStatus.Ready);
+            MessageData msgData = new MessageData(msgKey.Id, data);
+            return this._Tree.Add(msgKey, msgData);
+        }
+
+        public bool Delete(ulong id)
+        {
+            return false;
+        }
+
+        public byte[] Dequeue()
+        {
+            MessageKey theKey = this._Tree.Keys.First<MessageKey>(new Func<MessageKey, bool>(FindNextMessage));
+            MessageData theData = null;
+            if (this._Tree.TryGetValue(theKey, out theData))
+            {
+                theKey.Status = MessageStatus.Reserved;
+                return theData.Data;
+            }
+            return new byte[] { };    
+        }
+
+        private bool FindNextMessage(MessageKey theKey)
+        {
+            if (theKey.Status == MessageStatus.Ready)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
