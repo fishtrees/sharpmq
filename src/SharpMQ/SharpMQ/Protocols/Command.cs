3e6fd40396bcdb958ea1a74f33ee6257ff78ad38
fishtrees <fishtrees@f0fb3338-e2dd-dc1e-c2d3-7b6d7c6fd296>
2011-12-27 16:03:02 +0000

    add ProtocolFactory

--- src/SharpMQ/SharpMQ/Protocols/Command.cs
+++ src/SharpMQ/SharpMQ/Protocols/Command.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FyndSharp.Communication.Common;
 
 namespace SharpMQ.Protocols
 {
     [Serializable]
-    public abstract class Command
+    public abstract class Command : IMessage
     {
         public abstract byte[] CreateBytes();
+
+        public string Id
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string RepliedId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
---
+++ src/SharpMQ/SharpMQ/Protocols/ProtocolFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using FyndSharp.Communication.Protocols;
+
+namespace SharpMQ.Protocols
+{
+    public class ProtocolFactory : IProtocolFactory
+    {
+        public IProtocol CreateProtocol()
+        {
+            return new HeadBodyProtocol();
+        }
+    }
+}
